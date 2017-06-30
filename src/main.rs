4d39db5161fb32def861c1dc60f885437fb2cf08
Joe Roberts <joe@resin.io>
2017-06-30 16:49:20 +0100

    Implement support for multiple stocks

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::io::Read;
-use std::path::PathBuf;
 
 extern crate strfmt;
 use strfmt::strfmt;
@@ -23,16 +22,16 @@ fn main() {
             Arg::with_name("crypto")
                 .short("c")
                 .long("crypto")
-                .help("Enter the crypto")
+                .help("Enter the crypto(s)")
                 .takes_value(true)
-                .default_value("ethereum"),
+                .multiple(true)
+                .required(true),
         )
         .arg(
             Arg::with_name("format")
                 .short("f")
                 .long("format")
                 .help("Enter the format")
-                .long_help("longer")
                 .takes_value(true)
                 .default_value(
                     "{name}, {symbol}, {rank}, {price_usd}, {price_btc}, \
@@ -41,18 +40,41 @@ fn main() {
                      {percent_change_7d}, {last_updated}",
                 ),
         )
+        .arg(
+            Arg::with_name("separator")
+                .short("s")
+                .long("separator")
+                .help("Enter the separator")
+                .takes_value(true)
+                .default_value(" | "),
+        )
         .get_matches();
 
-    let mut url = PathBuf::from("http://api.coinmarketcap.com/v1/ticker/");
-    url.push(matches.value_of("crypto").unwrap());
+    let cryptos: Vec<_> = matches.values_of("crypto").unwrap().collect();
+    let format: &str = matches.value_of("format").unwrap();
+    let separator: &str = matches.value_of("separator").unwrap();
 
     let client = Client::new();
-    let mut response = client.get(url.to_str().unwrap()).send().unwrap();
+    let mut response = client
+        .get("http://api.coinmarketcap.com/v1/ticker/?limit=10")
+        .send()
+        .unwrap();
     let mut contents = String::new();
     response.read_to_string(&mut contents).unwrap();
 
     let stocks: Vec<HashMap<String, String>> = serde_json::from_str(&contents).unwrap();
-    let stock = stocks.first().unwrap();
+    let stocks: HashMap<String, HashMap<String, String>> = stocks
+        .iter()
+        .map(|s| (s["symbol"].clone(), s.clone()))
+        .collect();
 
-    println!("{}", strfmt(matches.value_of("format").unwrap(), stock).unwrap());
+    let mut iter = cryptos.iter().peekable();
+    while let Some(current) = iter.next() {
+        let stock = stocks.get::<str>(current).unwrap();
+        print!("{}", strfmt(format, stock).unwrap());
+        match iter.peek() {
+            Some(_) => print!("{}", separator),
+            None => println!(""),
+        }
+    }
 }
